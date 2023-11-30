21996b0fc7b4469dd555b27a6983df0a226148d0
Javier Varela <jvarela@strands.com>
2023-11-30 23:28:19 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./components/card";
+import { useSelector } from "react-redux";
+import { useAppDispatch, RootState, AppDispatch } from "./redux/index";
+import { fetchImages, Image } from "./redux/ImageReducer";
+
+// const text = [
+//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor ante ligula, id aliquet ligula laoreet id. In aliquam hendrerit mauris, at sagittis tortor efficitur non. Proin egestas facilisis augue.",
+//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor ante ligula, i",
+//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor ante ligula, id aliquet ligula laoreet id. In aliquam hendrerit mauris, at sagittis tortor efficitur non. Proin egestas facilisis augue. Sed aliquet maximus tempus. Nullam ornare efficitur blandit. Sed eu mattis tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor ante ligula, id aliquet ligula laoreet id. In aliquam hendrerit mauris, at sagittis tortor efficitur non. Proin egestas facilisis augue. Sed aliquet maximus tempus. Nullam ornare efficitur blandit. Sed eu mattis tortor",
+//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor ante ligula, i",
+//   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tempor ante ligula, i",
+// ];
+
+// const images = [
+//   "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+//   "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+//   "https://angular.io/assets/images/logos/angular/angular.png",
+//   "https://angular.io/assets/images/logos/angular/angular.png",
+//   "https://angular.io/assets/images/logos/angular/angular.png",
+// ];
+
+// const titles = ["imagen", "imagen 2", "imagen 3", "imagen 4", "imagen 5"];
 
 function App() {
+  // const [data, setData] = useState([1, 2, 3, 4, 5]);
+
+  const dispatch: AppDispatch = useAppDispatch();
+  const imageList = useSelector((state: RootState) => state.image.images);
+  const loading = useSelector((state: RootState) => state.image.loading);
+  const error = useSelector((state: RootState) => state.image.error);
+  useEffect(() => {
+    dispatch(fetchImages());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <span className="title">Unith Test</span>
+      <div className="container">
+        {imageList.map((el: Image) => (
+          <Card
+            image={el.image}
+            title={el.title}
+            index={el.index}
+            description={el.description}
+          />
+        ))}
+      </div>
     </div>
   );
 }
